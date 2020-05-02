c6ca615354f117a7489e5b024cd7bd4c0782d0b0
akeshreddy <44496922+akeshreddy@users.noreply.github.com>
2020-05-02 11:18:07 +0530

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -1,6 +1,6 @@
 # SNAKES GAME
 # Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
-
+#This is a funny game.
 import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
